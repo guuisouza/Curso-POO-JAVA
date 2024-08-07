a444a9807ee8d520a0d07e36b4f4512c9b090ba4
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-08-07 17:18:54 -0300

    (07/08) Adicionando pasta de projeto: Exemplo de Herança

---
+++ heranca-polimorfismo/src/exemploheranca/application/Program.java
@@ -0,0 +1,11 @@
+package exemploheranca.application;
+
+import exemploheranca.entities.BusinessAccount;
+
+public class Program {
+    public static void main(String[] args) {
+        BusinessAccount busAcc = new BusinessAccount();
+
+
+    }
+}
