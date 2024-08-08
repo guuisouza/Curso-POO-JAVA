3f8ee7966c484f1e11dc4154b7602250616c195e
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-08-08 16:54:23 -0300

    (08/08) Atualizando pasta de projeto: Sobreposição de métodos e @Override

---
+++ heranca-polimorfismo/src/sobreposicaoMetodos/application/Program.java
@@ -0,0 +1,21 @@
+package sobreposicaoMetodos.application;
+
+import sobreposicaoMetodos.entities.Account;
+import sobreposicaoMetodos.entities.BusinessAccount;
+import sobreposicaoMetodos.entities.SavingsAccount;
+
+public class Program {
+    public static void main(String[] args) {
+        Account acc1 = new Account(1001, "Alex", 1000.00);
+        acc1.withdraw(200.0);
+        System.out.println(acc1.getBalance());
+
+        Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
+        acc2.withdraw(200.0);
+        System.out.println(acc2.getBalance());
+
+        Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
+        acc3.withdraw(200.0);
+        System.out.println(acc3.getBalance());
+    }
+}
